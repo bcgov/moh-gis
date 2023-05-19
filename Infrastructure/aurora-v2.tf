a3db3d92b5d3fe0218572c01730b53da2da56ed6
nathanielcx <51494277+nathanielcx@users.noreply.github.com>
2023-05-19 07:26:47 -0700

    Update aurora-v2.tf

--- Infrastructure/aurora-v2.tf
+++ Infrastructure/aurora-v2.tf
@@ -38,7 +38,7 @@ resource "aws_db_subnet_group" "gis_subnet_group" {
 
 data "aws_rds_engine_version" "postgresql" {
   engine  = "aurora-postgresql"
-  version = "13.9"
+  version = "14.6"
   filter {
     name = "engine-mode"
     values = [ "serverless" ]
