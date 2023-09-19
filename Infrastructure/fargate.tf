5dc969705e59df6da70110b8d92cc611516f4664
ahoplock <68440291+ahoplock@users.noreply.github.com>
2023-09-19 11:10:30 -0500

    Enable aws_ecs_service force_new_deployment to force a new task deployment of the service when Docker image/tag combinations remain the same (gis:latest)

--- Infrastructure/fargate.tf
+++ Infrastructure/fargate.tf
@@ -109,6 +109,7 @@ resource "aws_ecs_service" "main" {
   desired_count                     = 1
   health_check_grace_period_seconds = 60
   wait_for_steady_state             = false
+  force_new_deployment              = true
 
   network_configuration {
     security_groups  = [data.aws_security_group.app.id]
