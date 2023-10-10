8e5e5d510755e638723f4f580c019cf79c8ffb5e
ahoplock <68440291+ahoplock@users.noreply.github.com>
2023-10-10 17:21:50 -0500

    Fix for "Unable to discern ProjectStage for value ${ENV=PROJECT_STAGE}."; Formatting

--- gis/GIS-war/src/main/java/giswar/listener/StartupListener.java
+++ gis/GIS-war/src/main/java/giswar/listener/StartupListener.java
@@ -8,7 +8,6 @@ import javax.servlet.ServletContextListener;
 import javax.sql.DataSource;
 import java.util.Properties;
 
-
 /**
  * Web application lifecycle listener.
  *
@@ -22,38 +21,27 @@ public class StartupListener implements ServletContextListener {
     @Resource(lookup = "java:app/jdbc/gis")
     private DataSource ds;
 
+    @Resource(lookup = "java:app/jsf/ProjectStage")
+    private String projectStage;
+
     @Resource(lookup = "java:app/gis/batch_properties")
     private Properties batchProperties;
-    
+
     private BatchJobAutoStarter batchJobAutoStarter;
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
 
+        String oldValue = projectStage;
+        projectStage = substituteEnvVariable(oldValue);
+
         for (String property : batchProperties.stringPropertyNames()) {
-            String value = batchProperties.getProperty(property);
-
-            String newValue = value.replace(" ", "");
-
-            // Check if the property contains a reference to an environment
-            // variable of the form ${ENV=VARIABLE_NAME}
-            if (newValue.startsWith(ENV_PREFIX) && newValue.endsWith(ENV_SUFFIX)) {
-                // Remove the prefix and suffix
-                String varName = newValue.substring(ENV_PREFIX.length(), newValue.length() - ENV_SUFFIX.length());
-
-                // Get the value of the environment variable
-                newValue = System.getenv(varName);
-
-                // Set the value of the property only if the env var is defined
-                if (newValue != null) {
-                    batchProperties.setProperty(property, newValue);
-                }
-            }
+            oldValue = batchProperties.getProperty(property);
+            batchProperties.setProperty(property, substituteEnvVariable(oldValue));
         }
 
         try {
             batchJobAutoStarter = new BatchJobAutoStarter(ds, batchProperties);
-            
         } catch (ConfigException e) {
             throw new RuntimeException(e);
         }
@@ -62,5 +50,26 @@ public class StartupListener implements ServletContextListener {
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
         batchJobAutoStarter.destroy();
+    }
+
+    private String substituteEnvVariable(String oldValue) {
+
+        String value = oldValue;
+        String newValue = oldValue.replace(" ", "");
+
+        // Check if the property contains a reference to an environment variable of the form ${ENV=VARIABLE_NAME}
+        if (newValue.startsWith(ENV_PREFIX) && newValue.endsWith(ENV_SUFFIX)) {
+            // Remove the prefix and suffix
+            String varName = newValue.substring(ENV_PREFIX.length(), newValue.length() - ENV_SUFFIX.length());
+
+            // Get the value of the environment variable
+            newValue = System.getenv(varName);
+
+            // Set the value of the property only if the env var is defined
+            if (newValue != null) {
+                value = newValue;
+            }
+        }
+        return value;
     }
 }
