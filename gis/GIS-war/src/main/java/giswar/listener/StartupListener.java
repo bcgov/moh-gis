31cda4852715966b76ecc5f57274dfba8534d8a1
ahoplock <68440291+ahoplock@users.noreply.github.com>
2023-10-19 16:09:14 -0500

    Fix for "Unable to discern ProjectStage for value ${ENV=PROJECT_STAGE}."

--- gis/GIS-war/src/main/java/giswar/listener/StartupListener.java
+++ gis/GIS-war/src/main/java/giswar/listener/StartupListener.java
@@ -21,9 +21,6 @@ public class StartupListener implements ServletContextListener {
     @Resource(lookup = "java:app/jdbc/gis")
     private DataSource ds;
 
-    @Resource(lookup = "java:app/jsf/ProjectStage")
-    private String projectStage;
-
     @Resource(lookup = "java:app/gis/batch_properties")
     private Properties batchProperties;
 
@@ -32,12 +29,24 @@ public class StartupListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
 
-        String oldValue = projectStage;
-        projectStage = substituteEnvVariable(oldValue);
-
         for (String property : batchProperties.stringPropertyNames()) {
-            oldValue = batchProperties.getProperty(property);
-            batchProperties.setProperty(property, substituteEnvVariable(oldValue));
+
+            String value = batchProperties.getProperty(property);
+            String newValue = value.replace(" ", "");
+
+            // Check if the value contains a reference to an environment variable of the form ${ENV=VARIABLE_NAME}
+            if (newValue.startsWith(ENV_PREFIX) && newValue.endsWith(ENV_SUFFIX)) {
+                // Remove the prefix and suffix
+                String varName = newValue.substring(ENV_PREFIX.length(), newValue.length() - ENV_SUFFIX.length());
+
+                // Get the value of the environment variable
+                newValue = System.getenv(varName);
+
+                // Set the value of the property only if the env var is defined
+                if (newValue != null) {
+                    batchProperties.setProperty(property, newValue);
+                }
+            }
         }
 
         try {
@@ -50,26 +59,5 @@ public class StartupListener implements ServletContextListener {
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
         batchJobAutoStarter.destroy();
-    }
-
-    private String substituteEnvVariable(String oldValue) {
-
-        String value = oldValue;
-        String newValue = oldValue.replace(" ", "");
-
-        // Check if the value contains a reference to an environment variable of the form ${ENV=VARIABLE_NAME}
-        if (newValue.startsWith(ENV_PREFIX) && newValue.endsWith(ENV_SUFFIX)) {
-            // Remove the prefix and suffix
-            String varName = newValue.substring(ENV_PREFIX.length(), newValue.length() - ENV_SUFFIX.length());
-
-            // Get the value of the environment variable
-            newValue = System.getenv(varName);
-
-            // Set the value to return only if the env var is defined
-            if (newValue != null) {
-                value = newValue;
-            }
-        }
-        return value;
     }
 }
