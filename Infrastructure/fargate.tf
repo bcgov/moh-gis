5347ddb4b5c1763e9b2aa2ff6b98c5d91957c76a
nathanielcx <51494277+nathanielcx@users.noreply.github.com>
2023-10-20 08:39:45 -0700

    Update fargate.tf

--- Infrastructure/fargate.tf
+++ Infrastructure/fargate.tf
@@ -104,7 +104,7 @@ resource "aws_ecs_service" "main" {
   force_new_deployment              = true
 
   triggers = {
-    redeployment = "${var.timestamp}"
+    redeployment = var.timestamp
   }
 
   network_configuration {
