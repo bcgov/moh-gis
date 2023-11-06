4930f3f167211155f5af95abcd3c45065f4fb88c
nathanielcx <51494277+nathanielcx@users.noreply.github.com>
2023-11-06 10:47:29 -0800

    Update sns.tf

--- Infrastructure/sns.tf
+++ Infrastructure/sns.tf
@@ -3,7 +3,7 @@ resource "aws_sns_topic" "gis-sns" {
 }
 
 resource "aws_sns_topic_subscription" "gis-email-target" {
-  topic_arn = gis-sns.topic.arn
+  topic_arn = gis-topic.topic.arn
   protocol  = "email"
   endpoint  = aws_secretsmanager_secret_version.gis_sns_email.arn
 }
