28d3b57702ccb41f7d440909f676ff19fb05ba29
nathanielcx <51494277+nathanielcx@users.noreply.github.com>
2023-11-06 10:25:49 -0800

    Update sns.tf

--- Infrastructure/sns.tf
+++ Infrastructure/sns.tf
@@ -1,3 +1,9 @@
 resource "aws_sns_topic" "gis-sns" {
   name = "gis-topic"
 }
+
+resource "aws_sns_topic_subscription" "gis-email-target" {
+  topic_arn = gis-sns.topic.arn
+  protocol  = "email"
+  endpoint  = "Java.vic@cgi.com"
+}
