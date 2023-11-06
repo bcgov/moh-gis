fbd3e963ae9a7172a559be7f130333147c270984
nathanielcx <nathanielc@uvic.ca>
2023-11-06 10:53:38 -0800

    Reffering to arn properly

--- Infrastructure/sns.tf
+++ Infrastructure/sns.tf
@@ -3,7 +3,7 @@ resource "aws_sns_topic" "gis-sns" {
 }
 
 resource "aws_sns_topic_subscription" "gis-email-target" {
-  topic_arn = gis-sns.topic.arn
+  topic_arn = aws_sns_topic.gis-sns.arn
   protocol  = "email"
   endpoint  = aws_secretsmanager_secret_version.gis_sns_email.arn
 }
